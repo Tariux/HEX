e39f628a2411668f3d76be9a974958087eb7aa0f
Tariux <tariux@protonmail.com>
2025-01-03 16:57:04 +0330

    update: package, clean

--- testMeow/test.micro.js.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const { _HEX } = require("..");
-const hexApp = new _HEX(path.join(__dirname, './example.domain/environments'));
-hexApp.launch();
-// hexApp.stop();
--- testMeow/user.micro.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const { _HEX } = require("..");
-const hexApp = new _HEX(path.join(__dirname, './app.user.managment/environments'));
-hexApp.launch();
-// hexApp.stop();
